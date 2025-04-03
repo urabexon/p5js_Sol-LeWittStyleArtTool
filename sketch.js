d89ce2b88744719cb23cee736eddc3ae8dc43469
urabexon <hiroki708.a48uk@gmail.com>
2025-04-03 23:55:19 +0900

    setup libraries init

---
+++ sketch.js
@@ -0,0 +1,7 @@
+const CRYSTAL_SIZE = 100;
+// layout
+let COLUMNS      = 10;
+let ROWS         = 6;
+let PADDING      = 0;//= CRYSTAL_SIZE * 0.2
+let GRIDBOX      = CRYSTAL_SIZE + PADDING;
+let START        = CRYSTAL_SIZE * 0.5;//(CRYSTAL_SIZE / 2) + MARGIN
