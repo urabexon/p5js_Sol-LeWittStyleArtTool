7f98d236dbd20c6c205844e846f42f31dd1c5060
urabexon <hiroki708.a48uk@gmail.com>
2025-05-16 16:46:53 +0900

    arc render setup

--- Layers.js
+++ Layers.js
@@ -1,3 +1,20 @@
 class arcShape extends Shapes {
-    constructor() {}
+    constructor(_x, _y, _strokeColor, _strokeW, _fillMode, _fillColor, _frameStrokeCl) {
+        super(_x, _y, _strokeColor, _strokeW, _fillMode, _fillColor, _frameStrokeCl);
+        this.id = floor(random(0, 4));
+    }
+
+    render() {
+        super.render();
+        push();
+        stroke(this.strokeColor);
+        strokeWeight(this.strokeW);
+        arc(this.posX, this.posY, this.gridSize, this.gridSize, this.randRot[this.id], this.randRot[this.id] + 180);
+        pop();
+    }
 }
+
+class semiArcShape extends Shapes {
+
+}
+
