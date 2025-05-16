7d1a7e5a9ff1d75c43911f458faa2426b4855888
urabexon <hiroki708.a48uk@gmail.com>
2025-05-16 16:54:37 +0900

    class constructor setup

--- Layers.js
+++ Layers.js
@@ -15,6 +15,40 @@ class arcShape extends Shapes {
 }
 
 class semiArcShape extends Shapes {
+    constructor(_x, _y, _strokeColor, _strokeW, _fillMode, _fillColor, _frameStrokeCl) {
+        super(_x, _y, _strokeColor, _strokeW, _fillMode, _fillColor, _frameStrokeCl);
+        this.id = floor(random(0, 4));
+    }
+
 
 }
 
+class lineShape extends Shapes {
+    constructor(_x, _y, _strokeColor, _strokeW, _fillMode, _fillColor, _frameStrokeCl) {
+        super(_x, _y, _strokeColor, _strokeW, _fillMode, _fillColor, _frameStrokeCl);
+        this.id = floor(random(0, 4));
+    }
+
+
+}
+
+class noisyLineShape extends Shapes {
+    constructor(_x, _y, _strokeColor, _strokeW, _fillMode, _fillColor, _frameStrokeCl) {
+        super(_x, _y, _strokeColor, _strokeW, _fillMode, _fillColor, _frameStrokeCl);
+        noiseSeed(random(this.gridSize));
+        this.id = floor(random(0, 4));
+        this.rand = random(0.005, 0.03);
+        this.yThres = floor(random(7, 16));
+    }
+
+
+}
+
+class dashedLineShape extends Shapes {
+    constructor(_x, _y, _strokeColor, _strokeW, _fillMode, _fillColor, _frameStrokeCl) {
+        super(_x, _y, _strokeColor, _strokeW, _fillMode, _fillColor, _frameStrokeCl);
+        this.id = floor(random(0, 4));
+    }
+
+    
+}
