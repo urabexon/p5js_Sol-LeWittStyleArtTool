4904ea8b69e4b145ea03396abdc48717f0d7376e
urabexon <hiroki708.a48uk@gmail.com>
2025-05-17 16:34:41 +0900

    Major change from svg to png save

--- GUI.js
+++ GUI.js
@@ -1,15 +1,15 @@
 class GUI {
     constructor() {
-         // GUI
-         let displayGridOnChange;
-         let thicknessOnchange;
-         let chanceHalfCircleOnChange;
-         let chanceLineOnChange;
-         let chanceDashedLineOnChange;
-         let chanceNoisyLineChange;
-         let chanceMidHalfCircleChange;
+        // GUI
+        let displayGridOnChange;
+        let thicknessOnchange;
+        let chanceHalfCircleOnChange;
+        let chanceLineOnChange;
+        let chanceDashedLineOnChange;
+        let chanceNoisyLineChange;
+        let chanceMidHalfCircleChange;
 
-         this.parameters = function () {
+        this.parameters = function () {
             this.displayGrid = true;
             this.percentHalfCircle = 75.0;
             this.percentStraightLine = 80.0;
@@ -22,77 +22,43 @@ class GUI {
             }
 
             this.Save = function () {
-                const w = width;
-                const h = height;
-
-                // 一時的にSVGキャンバスを作成
-                let svgP5 = new p5((p) => {
-                    p.setup = function () {
-                        p.createCanvas(w, h, SVG);
-                        p.noFill();
-                        p.background('#1c1c1c');
-
-                        // 一時的に SHAPES をコピーして再描画
-                        for (let i = 0; i < SHAPES.length; i++)
-                            SHAPES[i].render(p);
-                        // 保存してインスタンス削除
-                        p.saveSVG('artwork.svg');
-                        setTimeout(() => {
-                            svgP5.remove();  // SVG Canvasごと削除
-                        }, 200);
-                    };
-                }, 'svg-container');
+                saveCanvas('artwork', 'png');
             }
+
             this.thickness = 1;
         }
 
         // GUI
         var gui = new dat.GUI({ name: 'Sol Lewitt Wall Drawings Generator', width: '27' });
         this.p = new this.parameters();
+
         displayGridOnChange = gui.add(this.p, "displayGrid").name("Display Bg Grid");
-        displayGridOnChange.onChange(function (value) {
-            draw();
-        });
+        displayGridOnChange.onChange(() => draw());
 
         thicknessOnchange = gui.add(this.p, "thickness").name("Stroke Thickness").min(1).max(8).step(1);
-        thicknessOnchange.onFinishChange(function (value) {
+        thicknessOnchange.onFinishChange((value) => {
             clear();
             for (let i = 0; i < SHAPES.length; i++)
                 SHAPES[i].setStrokeW(value);
             draw();
         });
 
-        // Half circle chance
         chanceHalfCircleOnChange = gui.add(this.p, "percentHalfCircle").min(0).max(100);
-        chanceHalfCircleOnChange.onChange(function (value) {
-            generateArcs();
-        });
+        chanceHalfCircleOnChange.onChange(() => generateArcs());
 
-        // Straight Line Shape
         chanceLineOnChange = gui.add(this.p, "percentStraightLine").min(0).max(100);
-        chanceLineOnChange.onChange(function (value) {
-            generateArcs();
-        });
+        chanceLineOnChange.onChange(() => generateArcs());
 
-        // Dashed Line Shape
         chanceDashedLineOnChange = gui.add(this.p, "percentDashedLine").min(0).max(100);
-        chanceDashedLineOnChange.onChange(function (value) {
-            generateArcs();
-        });
+        chanceDashedLineOnChange.onChange(() => generateArcs());
 
-        // chanceNoisyLineChange Line Shape
         chanceNoisyLineChange = gui.add(this.p, "percentNoisyLine").min(0).max(100);
-        chanceNoisyLineChange.onChange(function (value) {
-            generateArcs();
-        });
+        chanceNoisyLineChange.onChange(() => generateArcs());
 
-        // chanceMidHalfCircleChange Line Shape
         chanceMidHalfCircleChange = gui.add(this.p, "percentMidHalfCircle").min(0).max(100);
-        chanceMidHalfCircleChange.onChange(function (value) {
-            generateArcs();
-        });
+        chanceMidHalfCircleChange.onChange(() => generateArcs());
 
         gui.add(this.p, "generateArt").name("Generate Artwork");
-        gui.add(this.p, "Save").name("Save As SVG");
+        gui.add(this.p, "Save").name("Save As PNG");
     }
 }
